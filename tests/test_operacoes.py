17c4cdc08ac36cc96cb492bd2722c23d1761bbe3
Iris <irisatooli@gmail.com>
2026-08-26 18:56:16 -0300

    Simula cobertura abaixo do limite

--- tests/test_operacoes.py
+++ tests/test_operacoes.py
@@ -12,18 +12,3 @@ def test_soma():
     assert soma(2, 3) == 5
 
 
-def test_subtracao():
-    assert subtracao(5, 3) == 2
-
-
-def test_multiplicacao():
-    assert multiplicacao(4, 3) == 12
-
-
-def test_divisao():
-    assert divisao(10, 2) == 5
-
-
-def test_divisao_por_zero():
-    with pytest.raises(ValueError):
-        divisao(10, 0)
